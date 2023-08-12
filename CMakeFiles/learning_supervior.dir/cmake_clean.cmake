1a953975dfb6f74cc011d6cbbef6de0cacd91b38
PinkShirmp <nminhtue094@gmail.com>
2023-08-12 22:04:27 +0700

    Finish project

---
+++ CMakeFiles/learning_supervior.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learning_supervior.dir/src/main.cpp.o"
+  "CMakeFiles/learning_supervior.dir/src/main.cpp.o.d"
+  "learning_supervior"
+  "learning_supervior.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learning_supervior.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
